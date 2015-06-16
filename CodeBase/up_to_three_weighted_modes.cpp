415924f727d3d96066c0f47bb273b43c19c3956b
Reid Rumelt <rbr73@cornell.edu>
2015-06-16 15:20:09 -0400

    Test two

--- CodeBase/up_to_three_weighted_modes.cpp
+++ CodeBase/up_to_three_weighted_modes.cpp
@@ -1,7 +1,7 @@
 #include <Rcpp.h>
 #include <string.h>
 
-// Test
+// Test two
 
 // [[Rcpp::export]]
 Rcpp::CharacterVector up_to_three_weighted_modes(Rcpp::CharacterVector labels, Rcpp::NumericVector probs, double threshold=0, bool inclusive=true)
